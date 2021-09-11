535b51159df6f11e54bfdcc0e5505e943d3b04b4
yocosaka <sakayoco55@gmail.com>
2021-09-11 16:23:16 +0900

    Add Material icons to display edit/delete/done icons for each project

--- README.md
+++ README.md
@@ -25,6 +25,9 @@ npm install -g json-server
 npx json-server --watch data/db.json
 ```
 
+<!-- ### 
+-  Google Material icons:　https://fonts.google.com/icons?selected=Material+Icons
+ -->
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
       <h3 @click="toggleShowDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
     </div>
     <div class="details" v-show="showDetails">
       <p>{{ project.details }}</p>
@@ -37,5 +42,19 @@ export default {
   }
   h3 {
     cursor: pointer;
+  }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+  }
+  .material-icons:hover {
+    color: #777;
   }
 </style>
