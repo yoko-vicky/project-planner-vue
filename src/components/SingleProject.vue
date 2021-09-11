17d0a11675878061a28483aae57fe3ac5ac1a150
yocosaka <sakayoco55@gmail.com>
2021-09-11 14:35:15 +0900

    Apply toggleShowDetails to SingleProject component

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,16 +1,41 @@
 <template>
-  <h4>{{ project.title }}</h4>
-  <p>{{ project.details }}</p>
-  <p>{{ project.complete }}</p>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="toggleShowDetails">{{ project.title }}</h3>
+    </div>
+    <div class="details" v-if="showDetails">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'SingleProject',
-  props: ['project']
+  props: ['project'],
+  data(){
+    return {
+      showDetails: false
+    }
+  },
+  methods: {
+    toggleShowDetails(){
+      this.showDetails = !this.showDetails
+    }
+  }
 }
 </script>
 
 <style>
-
+.project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+    border-left: 4px solid #e90074;
+  }
+  h3 {
+    cursor: pointer;
+  }
 </style>
