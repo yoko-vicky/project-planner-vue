03a9d8f6e2ec60703530793bd41ce8bc44648e95
yocosaka <sakayoco55@gmail.com>
2021-09-12 17:16:35 +0900

    Install express & serve-static and Create server.js to setup express server - Move common global style for edit & add forms in assets/global.css and import it in main.js

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -49,42 +49,5 @@ export default {
 </script>
 
 <style>
-form {
-    background: white;
-    padding: 20px;
-    border-radius: 10px;
-  }
-  label {
-    display: block;
-    color: #bbb;
-    text-transform: uppercase;
-    font-size: 14px;
-    font-weight: bold;
-    letter-spacing: 1px;
-    margin: 20px 0 10px 0
-  }
-  input {
-    padding: 10px;
-    border: 0;
-    border-bottom: 1px solid #ddd;
-    width: 100%;
-    box-sizing: border-box;
-  }
-  textarea {
-    border: 1px solid #ddd;
-    padding: 10px;
-    width: 100%;
-    box-sizing: border-box;
-    height: 100px;
-  }
-  form button {
-    display: block;
-    margin: 20px auto 0;
-    background: #00ce89;
-    color: white;
-    padding: 10px;
-    border: 0;
-    border-radius: 6px;
-    font-size: 16px;
-  }
+
 </style>
