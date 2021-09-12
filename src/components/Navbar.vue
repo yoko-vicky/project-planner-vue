12757334b7d2cca8be29841ec1b0eafd6eb86e34
yocosaka <sakayoco55@gmail.com>
2021-09-12 10:54:06 +0900

    Add style for Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/add">Add Project</router-link>
+  <div class="main-nav">
+    <router-link :to="{ name: 'Home'}">Projects</router-link> |
+    <router-link :to="{ name: 'AddProject'}">Add a New Project</router-link>
   </div>
 </template>
 
@@ -12,5 +12,19 @@ export default {
 </script>
 
 <style>
-
+  .main-nav {
+    text-align: center;
+    margin: 40px auto;
+  }
+  .main-nav a{
+    display: inline-block;
+    text-decoration: none;
+    margin: 0 10px;
+    color: #999;
+    font-size: 18px;
+  }
+  a.router-link-active {
+    border-bottom: 2px solid #00ce89;
+    padding-bottom: 4px;
+  }
 </style>
